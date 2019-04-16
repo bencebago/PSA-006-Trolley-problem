f2d5c3025d52d0c224de61c5ae984a2af0c56870
nthun <thomassocska@yahoo.com>
2019-04-16 13:21:01 +0200

    Refactor function for efficiency

--- R/generate_datasets.R
+++ R/generate_datasets.R
@@ -1,9 +1,9 @@
-# Generate dataset
-# 
+# Generate a dataset of integer values with specified distribution paramaters
+# OUTPUT: A tibble in long format
+# EXAMPLE: generate_dataset(n = 1000, mean = 5, sd = 2, tasks = 4, groups = c("a", "b"))
 
 
 # TODO: add parameters (vars, smd) for variables that should be different
-# TODO: Generate an already tidy dataset
 
 generate_dataset <- function(n, 
                              mean, 
@@ -13,18 +13,21 @@ generate_dataset <- function(n,
                              tasks = 4, 
                              groups = c("a", "b", "c")){
   # Gerate  the variable names. Values are added as a list of NA values to be later unnested
-  tidyr::crossing(task = stringr::str_c("v",1:tasks), 
-                  group = groups, 
-                  value = list(rep(NA_real_, n))) %>% 
-    tidyr::unite(key, task, group) %>% 
-    tidyr::spread(key, value) %>% 
-    tidyr::unnest() %>% 
-  # Add the random values
-    dplyr::mutate_all(~draw_values(n, mean, sd, floor, ceiling)) %>% 
-    # Gather data into long format
+  variables <-
+    paste(
+      paste("v", 1:tasks, sep = ""),
+      rep(groups, each = tasks),
+      sep = "_"
+    )
+
+  map(variables, 
+      ~draw_values(n, mean, sd, floor, ceiling)) %>% 
+    set_names(variables) %>% 
+    as_tibble() %>% 
     gather(code, choice)
 
 }
+
 
 # Test if the function generates the dataset
 # 
@@ -34,10 +37,12 @@ generate_dataset <- function(n,
 # 
 # df_test <- generate_dataset(n = 3000, mean = 5.2, sd = 2.19, tasks = 5, groups = c("x", "y", "w", "z"))
 # 
-# df_test %>% 
-#   purrr::map(~t.test(.x, mu = 5.2) %>% 
-#               broom::tidy()) %>% 
-#   dplyr::bind_rows()
+# df_test %>%
+#   dplyr::group_by(code) %>%
+#   tidyr::nest() %>% 
+#   dplyr::mutate(result = purrr::map(data, ~t.test(pull(.x), mu = 5.2) %>%
+#                                            broom::tidy())) %>%
+#   tidyr::unnest(result)
 
  
   
