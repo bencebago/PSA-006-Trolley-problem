5d6594ca86f37dfd83ee2f6759782876f9af9848
nthun <thomassocska@yahoo.com>
2019-05-06 00:06:04 +0200

    Add dependencies

--- R/get_bf_inference.R
+++ R/get_bf_inference.R
@@ -7,27 +7,33 @@
 #        bf_tresholds: the inference will be based on these
 # OUTPUT: A tibble with the grouping variables, a calculated BF, and the inference
 # EXAMPLE:
-# evaluate_results(full_data2, groups = c("study", "class"), formula = choice ~ condition, prior = "medium")
+# get_bf_inference(full_data2, groups = c("study", "class"), formula = choice ~ condition, prior = "medium")
 
-# TODO: Getting the BF from the object is clumsy
+# TODO: Getting the BF from the object is clumsy, is there a more straightforward way?
+
+if (!require(dplyr)) install.packages("dplyr")
+if (!require(purrr)) install.packages("purrr")
+if (!require(rlang)) install.packages("rlang")
 
 get_bf_inference <- function(df, 
                              groups = c("study", "class"),
                              formula = NULL,
                              prior = prior,
-                             bf_thresholds = c(3, 1/3)
+                             # Evidence for the alternative and null, respectively
+                             bf_thresholds = c(5, 1/5) 
                              ){
 
   df %>% 
-    group_nest(!!!syms(groups)) %>%
-    transmute(group_cols(), 
-              bf = map_dbl(data, ~ttestBF(formula = formula, 
-                                          rscale = prior, 
-                                          data = as.data.frame(.x)) %>% 
-                             matrix() %>% 
-                             as.numeric()),
-              inference = case_when(bf > bf_thresholds[1] ~ "replicated",
-                                    bf < bf_thresholds[2] ~ "not replicated",
-                                    TRUE ~ "inconclusive"))
+    dplyr::group_nest(!!!syms(groups)) %>%
+    dplyr::transmute(!!!syms(groups), 
+                     bf = purrr::map_dbl(data, 
+                                         ~BayesFactor::ttestBF(formula = formula, 
+                                                               rscale = prior, 
+                                                               data = as.data.frame(.x)) %>% 
+                                         matrix() %>% 
+                                         as.numeric()),
+                     inference = dplyr::case_when(bf > bf_thresholds[1] ~ "replicated",
+                                                  bf < bf_thresholds[2] ~ "not replicated",
+                                                  TRUE ~ "inconclusive"))
 }
 
