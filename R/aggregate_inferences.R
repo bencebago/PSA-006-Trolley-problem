2a236ac0a353badd62be44d223ec64922bc61080
nthun <thomassocska@yahoo.com>
2019-05-06 00:06:24 +0200

    Add package names

--- R/aggregate_inferences.R
+++ R/aggregate_inferences.R
@@ -1,16 +1,31 @@
 # Create summary table
+# Summarise the result of several simulations
+# INPUT: a list of replications, created by the simulation function
+# OUTPUT: a tibble that shows correct and incorrect inference rates for different scenarios
+# EXAMPLE: aggregate_inferences(out)
+
+if (!require(purrr)) install.packages("purrr")
+if (!require(furrr)) install.packages("furrr") # Faster for large (10k+) samples
+if (!require(dplyr)) install.packages("dplyr")
+if (!require(tibble)) install.packages("tibble")
 
 aggregate_inferences <- function(ls){
 
   ls %>% 
-  map_dfr(out, ~enframe(.x, name = NULL) %>% 
-            t() %>% 
-            as_tibble()) %>%
-  rename(true_effect = 1, 
-         sample_size_per_class = 2, 
-         correct_inference_rate = 3,
-         incorrect_inference_rate = 4) %>% 
-  group_by(true_effect, sample_size_per_class) %>% 
-  summarise(correct_inference_rate = mean(as.logical(correct_inference_rate)),
-            incorrect_inference_rate = mean(as.logical(incorrect_inference_rate)))
+    furrr::future_map_dfr(., 
+                          ~tibble::enframe(.x, name = NULL) %>% 
+                           t() %>% 
+                           tibble::as_tibble()) %>%
+    dplyr::rename(
+                   true_effect = 1, 
+                   sample_size_per_class = 2, 
+                   study = 3,
+                   correct_inference_rate = 4,
+                   incorrect_inference_rate = 5) %>% 
+    dplyr::mutate(sample_size_per_class = as.numeric(sample_size_per_class)) %>%
+    dplyr::group_by(true_effect, sample_size_per_class, study) %>% 
+    dplyr::summarise(correct_inference_rate = mean(as.logical(correct_inference_rate)),
+                     incorrect_inference_rate = mean(as.logical(incorrect_inference_rate))
+                     ) %>% 
+    dplyr::ungroup()
 }
