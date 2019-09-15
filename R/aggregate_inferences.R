9e67864826a32d4bd2bca4d01bce3114c303f930
nthun <thomassocska@yahoo.com>
2019-09-15 17:31:06 +0200

    Removed a parameter

--- R/aggregate_inferences.R
+++ R/aggregate_inferences.R
@@ -17,13 +17,12 @@ aggregate_inferences <- function(ls){
                            t() %>% 
                            tibble::as_tibble()) %>%
     dplyr::rename(
-                   true_effect = 1, 
-                   sample_size_per_class = 2, 
-                   study = 3,
-                   correct_inference_rate = 4,
-                   incorrect_inference_rate = 5) %>% 
+                   sample_size_per_class = 1, 
+                   study = 2,
+                   correct_inference_rate = 3,
+                   incorrect_inference_rate = 4) %>% 
     dplyr::mutate(sample_size_per_class = as.numeric(sample_size_per_class)) %>%
-    dplyr::group_by(true_effect, sample_size_per_class, study) %>% 
+    dplyr::group_by(sample_size_per_class, study) %>% 
     dplyr::summarise(correct_inference_rate = mean(as.logical(correct_inference_rate)),
                      incorrect_inference_rate = mean(as.logical(incorrect_inference_rate))
                      ) %>% 
