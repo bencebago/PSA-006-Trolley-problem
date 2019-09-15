3d2117210002954bb6923fb8da17faa9c31d94a3
nthun <thomassocska@yahoo.com>
2019-09-15 17:32:15 +0200

    Changed the BF inference function

--- R/get_ttestbf_inference.R
+++ R/get_ttestbf_inference.R
@@ -7,9 +7,7 @@
 #        bf_tresholds: the inference will be based on these
 # OUTPUT: A tibble with the grouping variables, a calculated BF, and the inference
 # EXAMPLE:
-# get_bf_inference(full_data2, groups = c("study", "class"), formula = choice ~ condition, prior = "medium")
-
-# TODO: Getting the BF from the object is clumsy, is there a more straightforward way?
+# get_ttestbf_inference(full_data2, groups = c("study", "class"), formula = choice ~ condition, prior = "medium")
 
 if (!require(dplyr)) install.packages("dplyr")
 if (!require(purrr)) install.packages("purrr")
@@ -17,12 +15,12 @@ if (!require(rlang)) install.packages("rlang")
 if (!require(BayesFactor)) install.packages("BayesFactor")
 
 
-get_bf_inference <- function(df, 
+get_ttestbf_inference <- function(df, 
                              groups = c("study", "class"),
                              formula = NULL,
                              prior = prior,
                              # Evidence for the alternative and null, respectively
-                             bf_thresholds = c(5, 1/5) 
+                             bf_thresholds = c(10, 1/10) 
                              ){
   suppressMessages(
   df %>% 
@@ -32,8 +30,7 @@ get_bf_inference <- function(df,
                                          ~BayesFactor::ttestBF(formula = formula, 
                                                                rscale = prior, 
                                                                data = as.data.frame(.x)) %>% 
-                                         matrix() %>% 
-                                         as.numeric()),
+                                          BayesFactor::extractBF(onlybf = TRUE)),
                      inference = dplyr::case_when(bf > bf_thresholds[1] ~ "replicated",
                                                   bf < bf_thresholds[2] ~ "not replicated",
                                                   TRUE ~ "inconclusive"))
