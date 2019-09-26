74bb6f303a99b6e388f9d73aa51eccf069644ddc
nthun <thomassocska@yahoo.com>
2019-09-27 01:00:47 +0200

    Add design matrix as an argument and generalize the grouping

--- R/aggregate_inferences.R
+++ R/aggregate_inferences.R
@@ -9,20 +9,17 @@ if (!require(furrr)) install.packages("furrr") # Faster for large (10k+) samples
 if (!require(dplyr)) install.packages("dplyr")
 if (!require(tibble)) install.packages("tibble")
 
-aggregate_inferences <- function(ls){
+aggregate_inferences <- function(combinations, out_list){
 
-  ls %>% 
+  out_list %>% 
     furrr::future_map_dfr(., 
                           ~tibble::enframe(.x, name = NULL) %>% 
                            t() %>% 
                            tibble::as_tibble()) %>%
-    dplyr::rename(
-                   sample_size_per_class = 1, 
-                   study = 2,
-                   correct_inference_rate = 3,
-                   incorrect_inference_rate = 4) %>% 
-    dplyr::mutate(sample_size_per_class = as.numeric(sample_size_per_class)) %>%
-    dplyr::group_by(sample_size_per_class, study) %>% 
+    dplyr::rename(correct_inference_rate = 1,
+                  incorrect_inference_rate = 2) %>% 
+    dplyr::bind_cols(combinations, .) %>% 
+    dplyr::group_by(!!!syms(names(select(combinations, -sample)))) %>% 
     dplyr::summarise(correct_inference_rate = mean(as.logical(correct_inference_rate)),
                      incorrect_inference_rate = mean(as.logical(incorrect_inference_rate))
                      ) %>% 
