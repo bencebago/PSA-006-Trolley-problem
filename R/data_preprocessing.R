53fb5983d1e9fc23c9675732e5ce84d2c52232ed
nthun <thomassocska@yahoo.com>
2020-04-03 11:59:14 +0200

    Change the randomization variable pocessing, remove obsolete criteria

--- R/data_preprocessing.R
+++ R/data_preprocessing.R
@@ -22,9 +22,8 @@ qualtrics_api_credentials(api_key = qualtrics_api_key,
 trolley_raw <- 
   qualtrics_survey_ids %>% 
   mutate(data = map(survey_id ,~fetch_survey(surveyID = .x,
-                                 include_display_order = TRUE,
-                                 force_request = TRUE))) %>% 
-  filter(str_detect(survey_name, "Southern")) %>% 
+                                include_display_order = TRUE,
+                                force_request = TRUE))) %>% 
   unnest(data)
   
 glimpse(trolley_raw)
@@ -34,15 +33,20 @@ glimpse(trolley_raw)
 
 trolley <-
   trolley_raw %>% 
-  # Rename randomization variables
-  # rename(scenario1 = FL_24_DO, 
-  #        scenario2 = FL_22_DO) %>%
+  # Aggregate randomization variables
+  mutate(scenario1 = case_when(FL_24_DO_Footbridgepole == 1 ~ "Pole",
+                               FL_24_DO_Footbridgeswitch == 1 ~ "Switch",
+                               TRUE ~ NA_character_),
+         scenario2 = case_when(FL_22_DO_Standardswitch == 1 ~ "Standardswitch",
+                               FL_22_DO_Standardfootbridge == 1 ~ "Standardfootbridge",
+                               FL_22_DO_Loop == 1 ~ "Loop",
+                               FL_22_DO_Obstaclecollide == 1 ~ "Obstaclecollide",
+                               TRUE ~ NA_character_)) %>% 
   # Remove those who didn't finish the questionnaire
   filter(Progress == 100) %>% 
-  # Remove underage participants
-  filter(age_1 >= 18) %>% 
   # Exclude careless responders
-  filter_at(vars(starts_with("careless_")), all_vars(. != "Yes")) %>%
+  filter_at(vars(careless_1, careless_2), all_vars(. != "Yes")) %>%
+  filter(careless_3 != "No") %>%
   # Remove confused participants 
   filter(confusion != "By the time I answered the questions on the preceding page, I was still somewhat confused by the material.  I do not think I understood this material well enough to give reasonable answers to the questions I was asked.") %>%
   # Exclude those with familiarity of the topic
@@ -51,10 +55,9 @@ trolley <-
   filter(is.na(str_match(technical_problems, "[a-zA-Z ]+"))) %>% 
   # Remove those who can't tell which scenarios they saw
   left_join(correct_answers, by = "scenario1") %>%
-  filter(trolley_attention == trolley_answer &
-         speedboat_attention == speedboat_answer) %>% 
+  filter(trolley_attention == trolley_answer) %>% 
   select(-trolley_answer, -speedboat_answer)
 
-  
+
 
   
